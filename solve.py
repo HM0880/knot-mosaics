537a6ed9fe5150e3dd98210242f8c63840c64638
HM0880 <HM0880@users.noreply.github.com>
2022-09-26 13:38:13 -0400

    Update to support writing the satisfying assignments to STDOUT

--- solve.py
+++ solve.py
@@ -1,12 +1,20 @@
 """Get all solutions of a knot mosaic SAT formula with PySAT bindings [1,2].
 
 To run:
-  python3 solve.py <m> <n>
+    python3 solve.py <m> <n> <mode>
+
+  where
+    m is the number of rows in the board,
+    n is the number of columns in the board, and
+    mode is "c" (count) or "w" (write) the satisfying assignments
+
+////////////////
 
 For quick testing, I'd recommend only going up to m,n of at most 4.
-The output should look something like
 
-    $ python3 solve.py 4 4
+An example of counting the number of solutions is
+
+    $ python3 solve.py 4 4 c
 
     solver, Cadical       , solutions found, 2594, total time (seconds), 0:00:00.590696
     solver, Gluecard3     , solutions found, 2594, total time (seconds), 0:00:00.298003
@@ -17,11 +25,15 @@ The output should look something like
     solver, MapleChrono   , solutions found, 2594, total time (seconds), 0:00:00.628719
     [[etc.]]
 
+
+An example of writing the satisfying assignment ouptut is
+
+    $ python3 solve.py 2 1 w
+    1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 12 -13 -14 -15 -16 -17 -18 -19 -20 -21 -22
+
 Hannah Miller
 https://hm0880.github.io/
 September 2022
-
-2022-04-23 (started)
 
 [1] https://pysathq.github.io/
 [2] https://github.com/pysathq/pysat
@@ -40,12 +52,15 @@ import encode  # custom Python file to encode basic constraints
 ################################################################
 
 
-def solve(solver_name, solver, formula):
+def solve_ALLSAT(solver_name, solver, formula, mode="c"):
     """Given a solver name, a PySAT solver object [1], and the SAT
     formula itself, find all satisfying assignments of the formula.
-
     PySAT's `solve' routine returns True when the formula can be
     satisfied [2].
+
+    The mode is either counting the number of satisfying assignments
+    ("c"; default) or writing the satisfying assignments to the
+    terminal ("w").
 
     [1] https://pysathq.github.io/docs/html/api/solvers.html
     [2] https://pysathq.github.io/docs/html/api/solvers.html#pysat.solvers.Solver.solve
@@ -65,23 +80,25 @@ def solve(solver_name, solver, formula):
         if is_SAT:
             model = solver.get_model()  # "model" = current satisfying assignment
             solver.add_clause([-1 * x for x in model])  # add blocking clause
+            idx += 1  # increment the counter
 
-            # print(model)
-            variables_that_are_true = [str(x) for x in model if x > 0]
-            # print(' '.join(variables_that_are_true))
+            # Write the model (i.e., satisfying assignment)
+            if mode == "w":
+                print(" ".join([str(_) for _ in model]))
 
-            idx += 1  # increment
         else:
             stop = datetime.datetime.now()
-            print(
-                f"solver, {solver_name:14s}, solutions found, {idx:4d}, total time (seconds), {stop-start}"
-            )
+            if mode == "c":  # report the counted number of solutions
+                print(
+                    f"solver, {solver_name:14s}, solutions found, {idx:4d}, total time (seconds), {stop-start}"
+                )
 
 
 ################################################################
 
-# MapleCM slowest solver at ~20 seconds for full solve
-# Lingeling is 2nd slowest solver at ~5 seconds for full solve
+# For an ALLSAT solve of a 4x4 board,
+#   - MapleCM is the slowest solver at ~20 seconds
+#   - Lingeling is the second slowest solver at ~5 seconds
 
 list_of_solvers = [
     # solver name, PySAT solver object
@@ -100,17 +117,29 @@ list_of_solvers = [
     ["MinisatGH", pysat.solvers.MinisatGH()],
 ]
 
+
 ################################################################
+# Call from the command line
 
-# Call from command line
-board = encode.MakeBoard(sys.argv[1], sys.argv[2])
-formula = encode.encode_basic_constraints_for_suitably_connected_knot(board)
+if __name__ == "__main__":
+    # Get command line arguments A
+    A = sys.argv
+    m = sys.argv[1]
+    n = sys.argv[2]
+    try:
+        mode = sys.argv[3]
+    except:
+        mode = "c"  # default to "c" for counting
 
-# Try all solvers
-print("")
-for name, solver in list_of_solvers:
-    solve(name, solver, formula)
+    # Make the board and encode the formula
+    board = encode.MakeBoard(m, n)
+    formula = encode.encode_basic_constraints_for_suitably_connected_knot(board)
 
-# Run individual solvers
-solve('minisat',pysat.solvers.Minisat22(),formula)
-solve('glucose4',pysat.solvers.Glucose4(),formula)
+    # Run per the requested mode
+    if mode == "c":  # count satisfying assignments
+        for name, solver in list_of_solvers:
+            solve_ALLSAT(name, solver, formula, mode)
+    elif mode == "w":  # write satisfying assignments with MiniSat
+        solve_ALLSAT("minisat", pysat.solvers.Minisat22(), formula, mode)
+    else:  # raise error for unknown mode
+        raise ValueError(f"mode `{mode}` is not supported; see doc string for help")
